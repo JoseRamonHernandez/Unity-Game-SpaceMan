2d38a9826137a10971d1486d5076e5e9ef1d2918
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-09-30 23:26:38 -0600

    init scripts

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    // Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Método para saltar
+    void Jump()
+    {
+        
+    }
+}
