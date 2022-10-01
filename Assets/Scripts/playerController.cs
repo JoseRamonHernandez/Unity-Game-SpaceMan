466e92db91ab7534a1e452407c0c6626e312fddc
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-01 00:26:55 -0600

    modified scripts

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,7 +7,9 @@ public class playerController : MonoBehaviour
 
     // Variables del movimiento del personaje
     public float jumpForce = 6f;
-    private Rigidbody2D rigidBody;
+    Rigidbody2D rigidBody;
+
+    public LayerMask groundMask;
 
     void Awake()
     {
@@ -23,15 +25,34 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
-            Jump();
-        }
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        Jump();
     }
-    
+
     //Método para saltar
     void Jump()
     {
-        rigidBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+        if(IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    // Método encargado de regresar un valor si el personaje esta tocando el suelo
+    bool IsTouchingTheGround()
+    {
+        if(Physics2D.Raycast(this.transform.position, 
+                            Vector2.down, 
+                            1.5f, 
+                            groundMask))
+        {
+            //TODO: programar lógica de contacto con el suelo
+            return true;
+        }
+        else
+        {
+            //TODO: programar lógica de no contacto
+            return false;
+        }
     }
 }
