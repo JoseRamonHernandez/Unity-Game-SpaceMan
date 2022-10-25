462de7e3f8239cc089b2a6596445a23a340f5041
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-25 01:16:31 -0600

    Iniciar movimient de la plataforma

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Animator animator = GetComponent<Animator>();
+        animator.enable = true;
+    }
+}
